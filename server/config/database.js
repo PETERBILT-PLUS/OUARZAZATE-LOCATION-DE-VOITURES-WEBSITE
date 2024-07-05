042ba046c28418f224a1dcf042148d09ffaadc6a
PETERBILT-PLUS <v8engine4stroke@gmail.com>
2024-07-05 21:39:47 +0200

    this is the version that i have fix all the bugs may

--- server/config/database.js
+++ server/config/database.js
@@ -1,18 +1,19 @@
-    // strapi-api/config/database.js
-    module.exports = ({ env }) => ({
-      connection: {
-        client: 'postgres',
-        connection: {
-          host: env('DATABASE_HOST', 'aws-0-eu-central-1.pooler.supabase.com'),
-          port: env.int('DATABASE_PORT', 6543),
-          database: env('DATABASE_NAME', 'postgres'),
-          user: env('DATABASE_USERNAME', 'postgres.zldbygongcaqnyfdpfcp'),
-          password: env('DATABASE_PASSWORD', 'UJr5qP@tynCcUbj'),
-          schema: env('DATABASE_SCHEMA', 'public'), // Not required
-          ssl: {
-            rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
-          },
-        },
-        debug: false,
+// strapi-api/config/database.js
+module.exports = ({ env }) => ({
+  connection: {
+    client: 'postgres',
+    connection: {
+      host: env('DATABASE_HOST', 'aws-0-eu-central-1.pooler.supabase.com'),
+      port: env.int('DATABASE_PORT', 5432),
+      database: env('DATABASE_NAME', 'postgres'),
+      user: env('DATABASE_USERNAME', 'postgres.zldbygongcaqnyfdpfcp'),
+      password: env('DATABASE_PASSWORD', 'UJr5qP@tynCcUbj'),
+      schema: env('DATABASE_SCHEMA', 'public'), // Not required
+      ssl: {
+        rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
+        ca: env('DATABASE_CA_CERT'),  // Add this line to use the CA certificate
       },
-    });
+    },
+    debug: false,
+  },
+});
